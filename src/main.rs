8bb38b42b7c25dc02f6bea0c894d698741b410db
sontixyou <kengoviolinphoto@gmail.com>
2024-10-16 20:57:05 +0900

    add wave-git-cz command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::{Arg, ArgMatches, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let cli: ArgMatches = Command::new("wave-git-cz")
+        .author("sontixyou")
+        .version("0.0.1")
+        .about("Semantic Git commits")
+        .arg(
+            Arg::new("commit_body")
+                .value_name("TEXT")
+                .help("Input commit body")
+                .num_args(1)
+                .required(true),
+        )
+        .get_matches();
+    let text = cli.get_one::<String>("commit_body").unwrap();
+    print!("{:?}", text);
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
+    use predicates::prelude::*;
+
+    type TestResult = Result<(), Box<dyn std::error::Error>>;
+    #[test]
+    fn test_main_no_commit_body() -> TestResult {
+        let mut cmd = Command::cargo_bin("wave-git-cz")?;
+        cmd.assert()
+            .failure()
+            .stderr("error: the following required arguments were not provided:\n  <TEXT>\n\nUsage: wave-git-cz <TEXT>\n\nFor more information, try '--help'.\n");
+        Ok(())
+    }
+
+    #[test]
+    fn test_main_with_commit_body() -> TestResult {
+        let mut cmd = Command::cargo_bin("wave-git-cz")?;
+        cmd.arg("commit_body")
+            .assert()
+            .success()
+            // .stdout(predicate::eq(b"\"commit_body\"" as &[u8]));
+            .stdout(predicate::eq("\"commit_body\""));
+        Ok(())
+    }
 }
